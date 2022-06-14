9c8e33e6919525d6b2a2e42e2813cff218f83278
Leilaflores <97005648+Leilaflores@users.noreply.github.com>
2022-06-14 14:47:43 -0300

    Agregando Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import './navbar.css';
+
+function Navbar(){
+    return(
+        <header>
+            <button>Lorem</button>
+            <p>nosotros</p>
+            <a src="#">link</a>
+        </header>
+    )
+}
+
+export default Navbar;
