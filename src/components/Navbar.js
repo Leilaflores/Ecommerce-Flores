a7aab45566177528308c147534d2349feadb428f
Leilaflores <floresleila2411@gmail.com>
2022-06-15 20:54:04 -0300

    master

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import './Navbar.css';
+
+function Navbar(){
+    return(
+        <header>
+            <button>Lorem</button>
+            <p>nosotros</p>
+            <a src="#">link</a>
+        </header>
+    )
+}
+
+export default Navbar;
